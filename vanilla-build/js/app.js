e10fb2e357d68a148d1c163ff5a3ef893c05b10d
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2024-11-01 05:50:29 +0100

    A pretty deep dive into tic tac toes

---
+++ vanilla-build/js/app.js
@@ -0,0 +1,44 @@
+import Store from './store.js';
+import View from './view.js';
+
+const players = [
+  { id: 1, name: 'Player 1', iconClass: 'fa-x', colorClass: 'turquoise' },
+  { id: 2, name: 'Player 2', iconClass: 'fa-o', colorClass: 'yellow' },
+  // add more players as needed...
+];
+
+function init() {
+  const view = new View();
+  const store = new Store('t3-storage', players);
+  // tab state change
+  store.addEventListener('statechange', () => {
+    view.render(store.game, store.stats);
+  });
+
+  // different tab state changes
+  window.addEventListener('storage', () => {
+    view.render(store.game, store.stats);
+  });
+
+  view.bindResetEvent((event) => {
+    store.reset();
+  });
+  view.bindNewRoundEvent((event) => {
+    store.newRound();
+  });
+  view.bindPlayerMoveEvent((square) => {
+    const existingMove = store.game.currentGameMoves.find(
+      (move) => move.squareId === +square.id
+    );
+    if (existingMove) {
+      return;
+    }
+    // Advance to the next state by pushing a move to the moves array
+    store.playerMove(+square.id);
+  });
+
+  // initView();
+  view.render(store.game, store.stats);
+}
+
+window.addEventListener('load', init);
---
+++ vanilla-build/js/view.js
@@ -0,0 +1,154 @@
+class View {
+  $ = {};
+  $$ = {};
+  constructor(state) {
+    this.$.menu = this.#qs('[data-id="menu"]');
+    this.$.menuBtn = this.#qs('[data-id="menu-btn"]');
+    this.$.menuItems = this.#qs('[data-id="menu-items"]');
+    this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
+    this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
+    this.$.modal = this.#qs('[data-id="modal"]');
+    this.$.modalText = this.#qs('[data-id="modal-text"]');
+    this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
+    this.$.turn = this.#qs('[data-id="turn"]');
+    this.$.p1Wins = this.#qs('[data-id=p1-wins]');
+    this.$.p2Wins = this.#qs('[data-id=p2-wins]');
+    this.$.ties = this.#qs('[data-id=ties]');
+
+    this.$$.squares = this.#qsAll('[data-id="square"]');
+
+    // UI-only event listeners
+    this.$.menuBtn.addEventListener('click', (event) => {
+      this.#toggleMenu();
+    });
+  }
+
+  render(game, stats) {
+    const { playerWithStats, ties } = stats;
+    const {
+      currentPlayer,
+      currentGameMoves,
+      status: { isComplete, winner },
+    } = game;
+    this.#closeAll();
+    this.#clearMoves();
+    this.#updateScoreBoard(
+      playerWithStats[0].wins,
+      playerWithStats[1].wins,
+      ties
+    );
+    this.#initializeMoves(currentGameMoves);
+    
+    if (isComplete) {
+      this.#openModal(winner ? `${winner.name} wins!` : 'Tie!');
+      return;
+    }
+    this.#setTurnIndicator(currentPlayer);
+  }
+
+  /**
+   *
+   * Register all the event listeners
+   */
+
+  #updateScoreBoard(p1Wins, p2Wins, ties) {
+    this.$.p1Wins.innerText = `${p1Wins} wins`;
+    this.$.p2Wins.innerText = `${p2Wins} wins`;
+    this.$.ties.innerText = `${ties} ties`;
+  }
+
+  bindResetEvent(handler) {
+    this.$.resetBtn.addEventListener('click', handler);
+    this.$.modalBtn.addEventListener('click', handler);
+  }
+
+  bindNewRoundEvent(handler) {
+    this.$.newRoundBtn.addEventListener('click', handler);
+  }
+  bindPlayerMoveEvent(handler) {
+    this.$$.squares.forEach((square) => {
+      square.addEventListener('click', () => handler(square));
+    });
+  }
+  /**
+   * Dom helper methods
+   */
+  #openModal(message) {
+    this.$.modal.classList.remove('hidden');
+    this.$.modalText.innerText = message;
+  }
+
+  #closeAll() {
+    this.#closeModal();
+    this.#closeMenu();
+  }
+  #closeModal() {
+    this.$.modal.classList.add('hidden');
+  }
+  #clearMoves() {
+    this.$$.squares.forEach((square) => square.replaceChildren());
+  }
+
+  #closeMenu() {
+    this.$.menuItems.classList.add('hidden');
+    this.$.menuBtn.classList.remove('border');
+
+    const icon = this.$.menuBtn.querySelector('i');
+    icon.classList.add('fa-chevron-down');
+    icon.classList.remove('fa-chevron-up');
+  }
+  #toggleMenu() {
+    this.$.menuItems.classList.toggle('hidden');
+    this.$.menuBtn.classList.toggle('border');
+
+    const icon = this.$.menuBtn.querySelector('i');
+    icon.classList.toggle('fa-chevron-down');
+    icon.classList.toggle('fa-chevron-up');
+  }
+
+  #handlePlayerMove(squareEl, player) {
+    const icon = document.createElement('icon');
+    icon.classList.add('fa-solid', player.iconClass, player.colorClass);
+
+    squareEl.replaceChildren(icon);
+  }
+
+  #initializeMoves(moves) {
+    this.$$.squares.forEach((square) => {
+      const existingMove = moves.find((move) => move.squareId === +square.id);
+      if (existingMove) {
+        this.#handlePlayerMove(square, existingMove.player);
+      }
+    });
+  }
+
+  #setTurnIndicator(player) {
+    const icon = document.createElement('i');
+    const label = document.createElement('p');
+
+    icon.classList.add('fa-solid', player.colorClass, player.iconClass);
+
+    label.classList.add(player.colorClass);
+
+    label.innerText = `${player.name} you're up`;
+
+    this.$.turn.replaceChildren(icon, label);
+  }
+
+  #qs(selector, parent = document) {
+    const el = parent.querySelector(selector);
+    if (!el) {
+      throw new Error(`Element not found: ${selector}`);
+    }
+    return el;
+  }
+  #qsAll(selector) {
+    const elList = document.querySelectorAll(selector);
+    if (!elList) {
+      throw new Error(`Element list not found: ${selector}`);
+    }
+    return elList;
+  }
+}
+
+export default View;
