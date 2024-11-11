1fb65a394b60bf46a25480b190971414b99f480c
Temidayo Omoyajowo <temidayoomoyajowo@seamlesshr.com>
2024-11-11 21:56:15 +0100

    feat: setup react from scratch

---
+++ react-refactor/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV ?? 'development',
+  entry: './src/entrypoint.jsx',
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-react', { runtime: 'automatic' }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+};
